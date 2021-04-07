5d6620bf02837cf7c5c202acbcc42938bdc0c81d
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-04-07 10:29:52 -0300

    example updated

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,23 @@
 import './module-aliases';
 import { RoverUseCase } from './domain';
+//
+// Example
+//
+const rover1 = ['1 2 N', 'LMLMLMLMM'];
+const rover2 = ['3 3 E', 'MMRMMRMRRM'];
 
-const inputs = ['3 3 E', 'M M R MMRMRRM'];
+const rovers = [rover1, rover2];
 
 const { Move, InputParser } = RoverUseCase;
 
-const position = InputParser(inputs[0]).getPosition();
-const instructions = InputParser(inputs[1]).getInstructions();
+rovers.forEach((rover, index) => {
+  const position = InputParser(rover[0]).getPosition();
+  const instructions = InputParser(rover[1]).getInstructions();
 
-const finalPosition = Move({
-  position,
-  instructions
+  const finalPosition = Move({
+    position,
+    instructions
+  });
+
+  console.log(index, 'final: ', finalPosition);
 });
-
-console.log('final: ', finalPosition);
