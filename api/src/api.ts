7994e402d15ab13b2b57c49e5f95996b881a0018
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-04-09 11:12:53 -0300

    envs implemented

--- api/src/api.ts
+++ api/src/api.ts
@@ -1,11 +1,13 @@
 import './module-aliases';
 import Hapi from '@hapi/hapi';
-import { routes } from './modules/routes';
+import { routes, env } from './modules';
+
+const { api } = env;
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+    host: api.host,
+    port: api.port
   });
 
   server.route(routes);
