15ed61fbdfef978517ee26fe7699dddd956886c7
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-04-10 14:33:27 -0300

    styled components install

--- app/src/components/test.component.tsx
+++ app/src/components/test.component.tsx
@@ -1,3 +1,9 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+  border: 1px solid red;
+`;
+
 export const Wrapper: React.FC = ({ children }) => {
-  return <div>{children}</div>;
+  return <Div>{children}</Div>;
 };
