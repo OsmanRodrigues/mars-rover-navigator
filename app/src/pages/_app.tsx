c84d2ca65dd958dfc8d6893e0c8dedc89eff3324
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-04-10 14:56:27 -0300

    atomic design and global style implemented

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,5 +1,17 @@
+import { GlobalStyle } from '@atomic';
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalStyle />
+      <Head>
+        <title>Mars Rover Navigator</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -4,10 +4,6 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Mars Rover Navigator</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
       <Wrapper>
         <h1>Hello, mars!</h1>
       </Wrapper>
