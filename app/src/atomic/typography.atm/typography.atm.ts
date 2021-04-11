d3406ee9f706bc31503588956bcc07a28c5fee20
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-04-11 14:58:40 -0300

    typography atoms created

---
+++ app/src/atomic/typography.atm/typography.atm.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1``;
+export const H2 = styled.h2``;
+export const H3 = styled.h3``;
+export const H4 = styled.h4``;
+export const Label = styled.label``;
