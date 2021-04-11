73d24fbe18e585f186edb9f41d1d87175a76cfd3
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-04-11 15:05:17 -0300

    grid system setup

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import Head from 'next/head';
 import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { setConfiguration } from 'react-grid-system';
+
+setConfiguration({ maxScreenClass: 'xl', gridColumns: 12 });
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [currentTheme, setCurrentTheme] = useState<ThemeName>(ThemeName.Dark);
